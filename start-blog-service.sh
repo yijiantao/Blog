a2b1346ee2b78c4dc0f4cddb6e6bcf4f4b7ab69f
yijiantao <yijiantao@hotmail.com>
2021-12-12 22:57:32 +0800

    :tada: init blog

--- start-blog-service.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /root/project/Blog
-nohup npx hexo server -p 9999 &
-tail -f nohup.out
--- stop-blog-service.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-pid=$(ps -ef|grep hexo|grep -v grep|awk '{print $2}')
-if [ -n "$pid" ]
-then
-    echo "kill -9 pid:" $pid
-    kill -9 $pid
-fi
